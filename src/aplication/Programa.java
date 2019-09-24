01575d62a5f6917b16dbbde84e15ba4f8e85509a
cleonehenrique <henriquenet.sat@gmail.co>
2019-09-24 18:17:26 -0300

    Acrecentadada a Mns de Boa Tarde!

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -3,7 +3,8 @@ package aplication;
 public class Programa {
 
 	public static void main(String[] args) {
-System.out.println( "Bom Dia");
+System.out.println( "Bom Dia!");
+System.out.println( "Boa Tarde!");
 
 	}
 
